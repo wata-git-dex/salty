65aa22bee09ae7525ff5f77900680b4f3337f00e
wata-git-dex <cleanwataorg@gmail.com>
2026-08-19 12:13:48 -0700

    Refresh app preview assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'salty-shell-v3';
-const SHELL = ['./', './index.html', './styles.css?v=4', './app.js?v=3', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'salty-shell-v5';
+const SHELL = ['./', './index.html', './styles.css?v=5', './app.js?v=5', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
