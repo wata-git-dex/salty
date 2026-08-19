6de9b8c5c93a7cce159be780b3ba9306b19fc6e2
wata-git-dex <cleanwataorg@gmail.com>
2026-08-19 13:44:12 -0700

    Refresh Salty auth shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'salty-shell-v10';
-const SHELL = ['./', './index.html', './styles.css?v=10', './app.js?v=10', './manifest.webmanifest', './manifest-amber.webmanifest', './manifest-foam.webmanifest', './manifest-ocean.webmanifest', './icon.svg', './icon-ink.svg', './icon-amber.svg', './icon-foam.svg', './icon-ocean.svg'];
+const CACHE = 'salty-shell-v11';
+const SHELL = ['./', './index.html', './styles.css?v=11', './app.js?v=11', './manifest.webmanifest', './manifest-amber.webmanifest', './manifest-foam.webmanifest', './manifest-ocean.webmanifest', './icon.svg', './icon-ink.svg', './icon-amber.svg', './icon-foam.svg', './icon-ocean.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
