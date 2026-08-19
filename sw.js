7f1344581f8722bc78c352c7df0608009291b0f0
wata-git-dex <cleanwataorg@gmail.com>
2026-08-19 12:29:18 -0700

    Refresh scheduled session assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'salty-shell-v5';
-const SHELL = ['./', './index.html', './styles.css?v=5', './app.js?v=5', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'salty-shell-v6';
+const SHELL = ['./', './index.html', './styles.css?v=6', './app.js?v=6', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
