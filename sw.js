39da9f18b545fe2da85bf9582d70ae1b291e27e1
wata-git-dex <cleanwataorg@gmail.com>
2026-08-19 18:12:26 -0700

    Release Salty v28 friend-ready chat
    Update installed copies with the final chat and DM interface polish.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'salty-shell-v27';
-const SHELL = ['./', './index.html', './styles.css?v=27', './app.js?v=27', './manifest.webmanifest', './icon.svg', './icon-ink.svg', './icon-amber.svg', './icon-foam.svg', './icon-ocean.svg'];
+const CACHE = 'salty-shell-v28';
+const SHELL = ['./', './index.html', './styles.css?v=28', './app.js?v=28', './manifest.webmanifest', './icon.svg', './icon-ink.svg', './icon-amber.svg', './icon-foam.svg', './icon-ocean.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
