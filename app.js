dc116f9441baff8972f38ba5a89cda8324fa7c71
Cyrus Maroofian <sodium@Cyruss-Mac-Studio.local>
2026-08-25 07:43:57 -0700

    Fix mobile bottom scroll spacing

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const CONFIG = Object.freeze({
   emailOtpDigits: 8,
   vapidPublicKey: 'BA51gFp65k9tONl1nzm_DCnk9Xh6eAGHyeWi0RTvuSZQzRSnyAYJfUeW2WCi86IXnxIWcIFq7UOprumm3ssvMnI',
 });
-const APP_VERSION = '1.82';
+const APP_VERSION = '1.83';
 const CONSENT_VERSION = '1.0';
 const GUIDE_PATH = './docs/SODIUM_Quick_Start_Guide_V13.pdf';
 const MASTER_GUIDE_PATH = './docs/SODIUM_Master_Instruction_Manual_V1.pdf';
